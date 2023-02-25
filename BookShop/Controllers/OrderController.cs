80cdb0bf043a2eb0a3adcfa37c4df405761b71c1
duy <duy3167@gmail.com>
2023-02-25 17:34:25 +0700

    done order owner

--- BookShop/Controllers/OrderController.cs
+++ BookShop/Controllers/OrderController.cs
@@ -1,17 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookShop.Models;
+using BookShop.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace BookShop.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+		private readonly AppDBContext dbContext;
+		private string zone { get; set; }
+		public OrderController(AppDBContext dbContext)
+		{
+			this.dbContext = dbContext;
+			this.zone = "owner,admin";
+		}
+		public IActionResult Index()
         {
-            return View();
+			if (!Authentication.Instance.Authorization(HttpContext, this.zone)) return Unauthorized();
+
+			var orderList = dbContext.orders.Include("user").ToList();
+			return View(orderList);
         }
 
-        public IActionResult Detail()
+        public IActionResult Detail(int id)
         {
-            return View();
+			if (!Authentication.Instance.Authorization(HttpContext, this.zone)) return Unauthorized();
+
+			var orderDetailList = dbContext.orderDetails.Include("order").Include("book").Where(od => od.order.order_id == id).ToList();
+			return View(orderDetailList);
         }
-    }
+
+
+
+	}
 }
